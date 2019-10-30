5401f74f45b9ddc093c81237b867a32f6364002e
Steven Jackson <stevenpjackson0@gmail.com>
2019-10-30 14:01:28 -0500

    Add: Sorting functions

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
 /* 
     !Array Basics!
+    !Sorting Arrays!
     Move comment arrows
     to isolate and 
     manipulate code blocks.
 */
 
 
-//Basic Array
+//Basic Array & Sorting Tutorial Combined
 $learn = array('Conditionals', 'Arrays', 'Loops', 'HTML', 'CSS'); //Basic Array
-
+$learn [] = "Build something awesome";//Add value to the end of the array
+array_push($learn, 'Functions', 'Forms', 'Objects');//Add values to the end of the array
+array_unshift($learn, 'HTML', 'CSS');//Add elements to the beginning of array
+asort($learn);//Sort array by value without changing the key
+sort($learn);//Re-index array keys after sorting
+rsort($learn);//Sort array values in reverse order
+shuffle($learn);//Randomly sort values
+krsort($learn);//Sort by key type not mixed in reverse order
+ksort($learn);//Sort by keys if type not mixed
+echo 'you removed' . array_shift($learn); //Remove first array element echo element
+echo 'you removed' . array_pop($learn); //Remove last array eleemnt echo element
+var_dump($learn);
 /*<------
------->*/
+
 
 //Array var_dump
 var_dump($learn); //Var dump array
@@ -30,15 +42,11 @@ var_dump($learn);
 $learn = "Build something awesome";//Overwrite array with string variable 
 var_dump($learn);
 
-array_push($learn, 'Functions', 'Forms', 'Objects');//Add values to the end of the array
-array_unshift($learn, 'HTML', 'CSS');//Add elements to the beginning of array
 echo implode(" ", $learn);
 
-echo 'you removed array' . array_shift($learn); //Remove first array element echo element
-echo 'you removed array' . array_pop($learn); //Remove last array eleemnt echo element
 unset($learn[1], $learn[2]); //Remove array element by key
 //unset($learn); //Destroy the array
 $learn = array_values($learn); //Re-index array values
 $learn[0] = 'Email'; //Update the first element in the array by key
-
+------>*/
 ?>
